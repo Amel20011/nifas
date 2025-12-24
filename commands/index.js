1a01335af998667eef214c9e3a0765edfd65c731
Amel20011 <amelcns8@gmail.com>
2025-12-24 14:33:19 +0800

    Create index.js

---
+++ commands/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    main: require('./main'),
+    user: require('./user'),
+    download: require('./download'),
+    search: require('./search'),
+    fun: require('./fun'),
+    tools: require('./tools'),
+    owner: require('./owner')
+};
